570f5ac0289b834eaf9cb204746835e903780a71
birdslisten <justin.schultz37@gmail.com>
2023-08-18 14:19:14 -0700

    disabled next btn when entering score

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// List of variables created that are linked to elements found in the HTML
+
 const questionElement = document.getElementById("question");
 const answerButtons = document.querySelectorAll(".answer-btn");
 const nextButton = document.getElementById("next-btn");
@@ -6,11 +6,10 @@ const startButton = document.getElementById("start-btn");
 const timerElement = document.getElementById("timer");
 const saveButton = document.getElementById("saveButton");
 const scoreList = document.getElementById("scoreList");
+const restartButton = document.getElementById("restart-btn");
 
-// Sets the initial score to 0
 let score = 0;
 
-// The questions and answers that the user will see when interracting with the quiz
 const questions = [
     {
         question: "Commonly used data types do NOT include:",
@@ -60,9 +59,33 @@ let currentQuestionIndex = -1;
 let timerInterval;
 let timeLeft = 60;
 
-//The function that displays the questions and answers
+function startQuiz() {
+    currentQuestionIndex = 0;
+    showQuestion(questions[currentQuestionIndex]);
+    document.getElementById("quizContainer").style.display = "block";
+    document.getElementById('start-btn').style.display = 'none';
+    nextButton.disabled = true;
+    startButton.disabled = true;
+    startTimer();
+}
+
+function startTimer() {
+    timerInterval = setInterval(updateTimer, 1000);
+    updateTimer();
+}
+
+function updateTimer() {
+    timeLeft--;
+    if (timeLeft >= 0) {
+        timerElement.innerText = `Time left: ${timeLeft} seconds`;
+    } else {
+        clearInterval(timerInterval);
+        handleTimeUp();
+    }
+}
+
 function showQuestion(question) {
-    questionElement.innerText = question.question;
+    questionElement.textContent = question.question;
 
     answerButtons.forEach((button) => {
         button.removeEventListener("click", handleAnswerClick);
@@ -77,39 +100,6 @@ function showQuestion(question) {
     nextButton.disabled = true;
 }
 
-//This function runs when the start quiz button is click
-function startQuiz() {
-    currentQuestionIndex = 0;
-    showQuestion(questions[currentQuestionIndex]);
-    document.getElementById("quizContainer").style.display = "block"; // Show the quiz container
-    document.getElementById('start-btn').style.display = 'none'; // Removes start button once quiz begins
-    nextButton.disabled = true;
-    startButton.disabled = true;
-    startTimer();
-
-}
-
-function startTimer() {
-    timerInterval = setInterval(updateTimer, 1000);
-    updateTimer(); // Start the timer immediately
-}
-
-function updateTimer() {
-    timeLeft--;
-    if (timeLeft >= 0) {
-        timerElement.innerText = `Time left: ${timeLeft} seconds`;
-    } else {
-        clearInterval(timerInterval);
-        handleTimeUp();
-    }
-}
-
-function handleTimeUp() {
-    console.log("Time's up!");
-    clearInterval(timerInterval); // Stop the timer
-    nextQuestion();
-}
-// This function checks if the answer selected is true or false, and will increase score or subtract from time remaining
 function handleAnswerClick(answer) {
     answerButtons.forEach((button) => {
         button.removeEventListener("click", handleAnswerClick);
@@ -119,47 +109,52 @@ function handleAnswerClick(answer) {
     if (answer.correct === true) {
         nextButton.textContent = 'Correct!';
         score++;
-    } else {
-        nextButton.textContent = 'Wrong!';
-        timeLeft -= 2;
-        if (timeLeft < 0) {
-            timeLeft = 0;
-        }
     }
 
     nextButton.disabled = false;
 }
-// This make the quiz progress until all questions are answered, and displays the score HTML elements
+
 function nextQuestion() {
     currentQuestionIndex++;
     if (currentQuestionIndex < questions.length) {
         showQuestion(questions[currentQuestionIndex]);
         nextButton.disabled = true;
     } else {
-        console.log("Quiz completed.");
+        stopTimer();
+        alert('Quiz Completed!');
         document.getElementById("finalScore").style.display = "block";
         document.getElementById("score").textContent = score;
+        document.getElementById('next-btn').disabled = true;
     }
 }
-// Resets the score, time, question index, hides final score elements, and re-displays the start quiz elements.
+function stopTimer() {
+    clearInterval(timerInterval);
+}
+
+function handleTimeUp() {
+    alert('Oh no! You have run out of time.');
+    clearInterval(timerInterval);
+    restartQuiz();
+}
+
 function restartQuiz() {
+    // Reset variables and timer
     currentQuestionIndex = -1;
     score = 0;
     timeLeft = 60;
     clearInterval(timerInterval);
-    updateTimer();
 
-    // Hide and show the appropriate sections
+    // Hide and show appropriate sections
     document.getElementById("quizContainer").style.display = "none";
     document.getElementById("finalScore").style.display = "none";
     document.getElementById("savedScores").style.display = "none";
 
-    // Show the "Start Quiz" button again
+    // Show and enable the "Start Quiz" button
     document.getElementById("start-btn").style.display = "block";
     document.getElementById("start-btn").disabled = false;
+    document.getElementById('next-btn').disabled = true;
 }
 
-// The event listeners for the start, next, save, and reset buttons.
 startButton.addEventListener("click", startQuiz);
 nextButton.addEventListener("click", nextQuestion);
 saveButton.addEventListener("click", function () {
@@ -169,16 +164,16 @@ saveButton.addEventListener("click", function () {
         savedScoreItem.textContent = `${initials}: ${score}`;
         scoreList.appendChild(savedScoreItem);
 
-        // Clear input and reset score
+
         document.getElementById("initials").value = "";
         score = 0;
 
-        // Hide the score entry form
+
         document.getElementById("finalScore").style.display = "none";
 
-        // Show the saved scores section
+
         document.getElementById("savedScores").style.display = "block";
     }
 });
-const restartButton = document.getElementById("restart-btn");
+
 restartButton.addEventListener("click", restartQuiz);
